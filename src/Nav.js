45b7785f62ec8e47f6d1ca96e5f4842f34b4be95
Tom Duggan <tduggan@squarespace.com>
2019-10-08 09:20:02 -0400

    comments

--- src/Nav.js
+++ src/Nav.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+/*
+  TODO: create and import a scss file for the Nav styles
+
+  Use a scss variable for the orange color
+  Use a mixin for the hover styles on the clickable elements
+
+*/
+
 const Nav = () => (
   <div className='nav'>
     <Link to='/' className='title'>
